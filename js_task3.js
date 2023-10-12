a03472b53cbd1689316ebbb32d9ceef2ee64f233
SheetalKizhakel <91269785+SheetalKizhakel@users.noreply.github.com>
2023-10-12 15:45:29 +0530

    Update js_task3.js

--- js_task3.js
+++ js_task3.js
@@ -1,26 +1,26 @@
-const userInput1 = document.querySelector('#num1'); // Getting the user input element
+const userInput1 = document.querySelector('#num1'); 
 const userInput2 = document.querySelector('#num2');
-        const btn1 = document.querySelector('#btn1');//making the button the target
+        const btn1 = document.querySelector('#btn1');
         const btn2 = document.querySelector('#btn2');
 
-        btn1.addEventListener('click', multiply);//trigger function reverse on clicking
+        btn1.addEventListener('click', multiply);
         btn2.addEventListener('click',divide);
 
         function multiply() {
-            const inputText1 = userInput1.value; // Get the value entered by the user as a string.use.value to find value of parameter entered
-            let num1 = parseInt(inputText1); // Convert the string to a number
-            const inputText2 = userInput2.value; // Get the value entered by the user as a string.use.value to find value of parameter entered
+            const inputText1 = userInput1.value; 
+            let num1 = parseInt(inputText1);
+            const inputText2 = userInput2.value; 
             let num2 = parseInt(inputText2); 
             let res=num1*num2;
 
-            document.getElementById("result").innerHTML = res;//make the value fore reversed number textbox as reversed number
+            document.getElementById("result").innerHTML = res;
         }
         function divide() {
-            const inputText1 = userInput1.value; // Get the value entered by the user as a string.use.value to find value of parameter entered
-            let num1 = parseInt(inputText1); // Convert the string to a number
-            const inputText2 = userInput2.value; // Get the value entered by the user as a string.use.value to find value of parameter entered
+            const inputText1 = userInput1.value; 
+            let num1 = parseInt(inputText1); 
+            const inputText2 = userInput2.value; 
             let num2 = parseInt(inputText2); 
             let res=num1/num2;
 
-            document.getElementById("result").innerHTML = res;//make the value fore reversed number textbox as reversed number
+            document.getElementById("result").innerHTML = res;
         }
