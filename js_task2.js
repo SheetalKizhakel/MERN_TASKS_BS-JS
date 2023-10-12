a46e37cca8f33f781732759b9a030b4e4a0b4840
SheetalKizhakel <91269785+SheetalKizhakel@users.noreply.github.com>
2023-10-12 15:44:36 +0530

    Update js_task2.js

--- js_task2.js
+++ js_task2.js
@@ -1,10 +1,10 @@
-const userInput = document.querySelector('#fname'); // Getting the user input element
-        const btn = document.querySelector('#btn');//making the button the target
+const userInput = document.querySelector('#fname'); 
+        const btn = document.querySelector('#btn');
 
-        btn.addEventListener('click', sortword);//trigger function reverse on clicking
+        btn.addEventListener('click', sortword);
 
         function sortword() {
-            const inputText = userInput.value; // Get the value entered by the user as a string.use.value to find value of parameter entered
+            const inputText = userInput.value; 
             const sortedtext=inputText.split("").sort().join("");
-            document.getElementById("lname").value = sortedtext;//make the value fore reversed number textbox as reversed number
+            document.getElementById("lname").value = sortedtext;
         }
