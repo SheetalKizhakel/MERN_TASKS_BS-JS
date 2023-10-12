78e9b180277235ca9391c96492b8a35189975fc1
SheetalKizhakel <91269785+SheetalKizhakel@users.noreply.github.com>
2023-10-12 15:46:01 +0530

    Update js_task4.js

--- js_task4.js
+++ js_task4.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-const btn1 = document.querySelector('#btn1'); // Getting the "Show Date" button
+const btn1 = document.querySelector('#btn1'); 
 const btn2 = document.querySelector('#btn2');
-btn1.addEventListener('click', showd);//trigger function reverse on clicking
+btn1.addEventListener('click', showd);
 btn2.addEventListener('click',showt);
 function showd()
 {
@@ -10,12 +10,12 @@ function showd()
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
     let currentDate = `${day}/${month}/${year}`;
-    document.getElementById("result1").textContent = currentDate;//make the value fore reversed number textbox as reversed number
+    document.getElementById("result1").textContent = currentDate;
 }
 function showt()
 {
     
     const date = new Date();
-    document.getElementById("result2").textContent = date;//make the value fore reversed number textbox as reversed number
+    document.getElementById("result2").textContent = date;
 }
 });
